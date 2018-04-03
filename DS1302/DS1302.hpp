72bafce506de6d7fe944948b4c426d60a94c439d
Jason Frame <jason@onehackoranother.com>
2018-04-03 22:24:58 +0100

    fix tabs

--- DS1302/DS1302.hpp
+++ DS1302/DS1302.hpp
@@ -20,53 +20,53 @@
 #define DS1302_RAM_BURST_READ    0xFF
 
 struct DS1302_RawTime {
-	char tenSeconds, seconds;
-	char tenMinutes, minutes;
-	char tenHours, hours;
+    char tenSeconds, seconds;
+    char tenMinutes, minutes;
+    char tenHours, hours;
 };
 
 template <class T>
 class DS1302 : public T {
 public:
-	void write(uint8_t addr, uint8_t v) {
-		T::beginTransaction();
-		T::setOutputMode();
-		T::shiftOut(addr);
-		T::shiftOut(v);
-		T::endTransaction();
-	}
+    void write(uint8_t addr, uint8_t v) {
+        T::beginTransaction();
+        T::setOutputMode();
+        T::shiftOut(addr);
+        T::shiftOut(v);
+        T::endTransaction();
+    }
 
-	uint8_t read(uint8_t addr) {
-		T::beginTransaction();
-		T::setOutputMode();
-		T::shiftOut(addr | 0x01);
-		T::setInputMode();
-		uint8_t v = T::shiftIn();
-		T::endTransaction();
-		return v;
-	}
+    uint8_t read(uint8_t addr) {
+        T::beginTransaction();
+        T::setOutputMode();
+        T::shiftOut(addr | 0x01);
+        T::setInputMode();
+        uint8_t v = T::shiftIn();
+        T::endTransaction();
+        return v;
+    }
 
-	void getTime(DS1302_RawTime *out) {
-		uint8_t clockBuffer[8];
-		readBurst(DS1302_CLOCK_BURST, 8, clockBuffer);
-		out->tenSeconds = (clockBuffer[0] >> 4) & 0x07;
-		out->seconds = (clockBuffer[0] & 0x0F);
-		out->tenMinutes = (clockBuffer[1] >> 4) & 0x07;
-		out->minutes = (clockBuffer[1] & 0x0F);
-		out->tenHours = (clockBuffer[2] >> 4) & 0x03;
-		out->hours = (clockBuffer[2] & 0x1F);
-	}
+    void getTime(DS1302_RawTime *out) {
+        uint8_t clockBuffer[8];
+        readBurst(DS1302_CLOCK_BURST, 8, clockBuffer);
+        out->tenSeconds = (clockBuffer[0] >> 4) & 0x07;
+        out->seconds = (clockBuffer[0] & 0x0F);
+        out->tenMinutes = (clockBuffer[1] >> 4) & 0x07;
+        out->minutes = (clockBuffer[1] & 0x0F);
+        out->tenHours = (clockBuffer[2] >> 4) & 0x03;
+        out->hours = (clockBuffer[2] & 0x1F);
+    }
 
-	void readBurst(uint8_t addr, int count, char *out) {
-		T::beginTransaction();
-		T::setOutputMode();
-		T::shiftOut(addr | 0x01);
-		T::setInputMode();
-		while (count--) {
-			*(out++) = T::shiftIn();
-		}
-		T::endTransaction();
-	}
+    void readBurst(uint8_t addr, int count, char *out) {
+        T::beginTransaction();
+        T::setOutputMode();
+        T::shiftOut(addr | 0x01);
+        T::setInputMode();
+        while (count--) {
+            *(out++) = T::shiftIn();
+        }
+        T::endTransaction();
+    }
 };
 
 #endif
